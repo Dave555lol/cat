869781bf356d55a6c63281a9896690ac03947f1b
Dave555lol <58588215+Dave555lol@users.noreply.github.com>
2020-10-03 21:18:30 +0200

    Add files via upload

---
+++ act.java
@@ -0,0 +1,18 @@
+public class act {
+    
+    String color;
+    int age;
+    String name;
+    
+
+    void sleep(){
+        System.out.println("Sleeping");
+    }
+    void play(){
+        System.out.println("Playing");
+    }
+    void feed(){
+        System.out.println("Eating");
+    }
+
+}
---
+++ cat.java
@@ -0,0 +1,22 @@
+public class cat {
+
+    public static void main(String[] args) {
+      
+       Cat Bleq = new Cat();
+       Cat Tiger = new Cat();
+
+       Bleq.name = "Bleq";
+       Bleq.age = 2;
+       Bleq.color = "Black";
+
+       Bleq.feed();
+
+       
+       Tiger.name = "Tiger";
+       Tiger.age = 5;
+       Tiger.color = "Orange";
+
+       Tiger.sleep();
+    }
+
+}
